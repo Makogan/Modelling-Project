e4ed4c350b01f856cc64d58d2cc3b235b40a7789
camilo.talero <camilo.talero@ucalgary.ca>
2017-04-08 16:35:37 -0600

    I am giving up

--- Shaders/FragmentShader.glsl
+++ Shaders/FragmentShader.glsl
@@ -6,27 +6,14 @@ in vec3 vertexPos;
 out vec4 outColor;
 
 uniform vec4 color = vec4(1);
-uniform vec3 light = vec3(0,-10,100);
 uniform vec3 cameraPos;
+
+uniform vec3 lightDirection = vec3(-1,0,0);
+uniform vec3 lightColor = vec3(1,1,1);
+uniform float ambientIntensity = 0.6f;
 
 void main()
 {
-  outColor = color;
-
-		/*outColor = vec4(1);
-		//if(length(Normal==1)
-			vec3 l = vec3(light-vPos);
-			l = normalize(l);
-			vec3 c = vec3(color);
-			vec3 n = vec3(Normal);
-			vec3 e = cameraPos-vPos;
-			e = -normalize(e);
-			vec3 h = normalize(e+l);
-			if(dot(n,l)<0)
-				n=-n;
-			if(test)
-				outColor = vec4(vec3(0.01)*max(0,pow(dot(h,n),1)),1.f);
-			else
-    		outColor = vec4(c*(vec3(0.2)+max(0,dot(n,l))) +vec3(0.01)*max(0,pow(dot(h,n), 1)), 1);
-			*/
+    float diffuseIntensity = max(0.0f, dot(normalize(vec3(Normal)), -lightDirection));
+    outColor = color*vec4(lightColor*(ambientIntensity + diffuseIntensity), 1.0f);
 }
